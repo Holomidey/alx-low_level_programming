8454e862c177e88c9465433b15de4d980b1917c6
holomidey <olomidewasiu@yahoo.com>
2023-03-20 14:17:58 -0700

    task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - als task eight
+ *
+ * Return: 0
+*/
+
+int main(void)
+{
+	int i;
+	int m;
+
+	for (i = 0 ; i < 10 ; i++)
+	putchar(i + '0');
+	for (m = 'a' ; m <= 'f' ; m++)
+	putchar(m);
+	putchar('\n');
+	return (0);
+}
