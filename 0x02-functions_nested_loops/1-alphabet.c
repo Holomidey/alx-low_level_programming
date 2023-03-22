c036a708d0db7fb9d7089ced2765b4cb5bf4ab3f
holomidey <olomidewasiu@yahoo.com>
2023-03-22 16:19:55 -0700

    jgfdcvb

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_alphabelt - print all the alphabelt in lower case
+ */
+
+void print_alphabet(void);
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	       _putchar(letter)
+
+	putchar('\n')
+}
