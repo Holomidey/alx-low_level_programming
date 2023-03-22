b6861116961a61e5f7c230131ca9b5257e93228f
holomidey <olomidewasiu@yahoo.com>
2023-03-22 16:31:57 -0700

    kjggfdsdf

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -7,8 +7,8 @@ void print_alphabet(void);
 {
 	char letter;
 
-	for (letter = 'a'; letter <= 'z'; letter++)
-	       _putchar(letter)
+	for (letter = 'a'; letter <= 'Z'; letter++)
+	       _putchar(letter);
 
-	putchar('\n')
+	putchar('\n');
 }
