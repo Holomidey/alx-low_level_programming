bccd3ec81ebc58a8c1fcd73e8c7f1fca285029a5
holomidey <olomidewasiu@yahoo.com>
2023-03-22 15:37:58 -0700

    hjggbjnnn

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,15 +1,15 @@
-#include "main.h"
+#include "main"
+
 /**
- * main - check this code
- *
+ * main - print _putchar
+ * is written by holomidey
  * Return: 0
-*/
+ */
 
 int main(void)
 {
 _putchar('_');
 _putchar('p');
-_putchar('u');
 _putchar('t');
 _putchar('c');
 _putchar('h');
